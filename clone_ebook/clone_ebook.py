700f079b81943dbc69a86ff3b166fedee193fc1c
Chip Lukes <34627158+chiplukes@users.noreply.github.com>
2025-05-30 21:40:12 -0600

    screen grabbing and converting in two different loops

--- clone_ebook/clone_ebook.py
+++ clone_ebook/clone_ebook.py
@@ -9,7 +9,7 @@ from marker.output import text_from_rendered
 from .screen import screen_coordinates
 
 
-def clone_ebook_main(book_name=None, output_directory=None, max_pages=10):
+def clone_ebook_main(book_names_lst=None, output_directory=None, max_pages=10):
     sc = screen_coordinates.screen_coordinates()
 
     print("Use mouse to draw a rectangle around text region from ebook reader.")
@@ -18,51 +18,62 @@ def clone_ebook_main(book_name=None, output_directory=None, max_pages=10):
     print("Click mouse on next page button from ebook reader.")
     sc.select_next_page_button()
 
-    page_i = 1
-    pages_lst = []
-    screen_previous = None
-    while True:
-        print(f"Page {page_i}")
-        page_i += 1
-        screenshot = sc.get_screenshot()
-        # text = pytesseract.image_to_string(screenshot)
-        # pages_lst.append(f"{text.strip()}\n")
-        pages_lst.append(screenshot)
-        if (
-            (screen_previous is not None) and (screenshot == screen_previous)
-        ) or page_i > max_pages:
-            break
-        screen_previous = screenshot
-        sc.next_page()
-        time.sleep(2)
     output_directory.mkdir(parents=True, exist_ok=True)
-    pages_lst[0].save(
-        output_directory / f"{book_name}.pdf",
-        "PDF",
-        resolution=100.0,
-        save_all=True,
-        append_images=pages_lst[1:],
-    )
+    for book_name in book_names_lst:
 
-    converter = PdfConverter(
-        artifact_dict=create_model_dict(),
-    )
-    rendered = converter(str(output_directory / f"{book_name}.pdf"))
-    text, _, images = text_from_rendered(rendered)
+        print(f"Capturing book: {book_name}")
+        input("Open ebook reader and press enter to select text region")
+        page_i = 1
+        pages_lst = []
+        screen_previous = None
+        while True:
+            print(f"Page {page_i}")
+            page_i += 1
+            screenshot = sc.get_screenshot()
+            # text = pytesseract.image_to_string(screenshot)
+            # pages_lst.append(f"{text.strip()}\n")
+            pages_lst.append(screenshot)
+            if (
+                (screen_previous is not None) and (screenshot == screen_previous)
+            ) or page_i > max_pages:
+                break
+            screen_previous = screenshot
+            sc.next_page()
+            time.sleep(2)
+        output_book_directory = output_directory / book_name
+        output_book_directory.mkdir(parents=True, exist_ok=True)
+        pages_lst[0].save(
+            output_book_directory /f"{book_name}.pdf",
+            "PDF",
+            resolution=100.0,
+            save_all=True,
+            append_images=pages_lst[1:],
+        )
 
-    with open(output_directory / f"{book_name}.md", "wt", encoding="utf-8") as f:
-        f.write(text)
 
-    for x in images:
-        img_path = output_directory / f"{x}"
-        images[x].save(f"{str(img_path)!s}")
+    for book_name in book_names_lst:
+        print(f"Converting book: {book_name}")
+        output_book_directory = output_directory / book_name
 
-    extra_args = []
-    extra_args.append(f"--resource-path={str(output_directory)!s}")
+        converter = PdfConverter(
+            artifact_dict=create_model_dict(),
+        )
+        rendered = converter(str(output_book_directory / f"{book_name}.pdf"))
+        text, _, images = text_from_rendered(rendered)
 
-    pypandoc.convert_file(
-        output_directory / f"{book_name}.md",
-        "epub",
-        outputfile=output_directory / f"{book_name}.epub",
-        extra_args=extra_args,
-    )
+        with open(output_book_directory / f"{book_name}.md", "wt", encoding="utf-8") as f:
+            f.write(text)
+
+        for x in images:
+            img_path = output_book_directory / f"{x}"
+            images[x].save(f"{str(img_path)!s}")
+
+        extra_args = []
+        extra_args.append(f"--resource-path={str(output_book_directory)!s}")
+
+        pypandoc.convert_file(
+            output_book_directory / f"{book_name}.md",
+            "epub",
+            outputfile=output_book_directory / f"{book_name}.epub",
+            extra_args=extra_args,
+        )
--- main.py
+++ main.py
@@ -11,11 +11,11 @@ if __name__ == "__main__":
 
     # Optional argument for book name
     parser.add_argument(
-            "--book_name",
-            type=str,
-            default="ebook",
-            help="Name of the book to be created",
-        )
+        "--book_name",
+        type=str,
+        default="ebook",
+        help="Name of the book or books to be created. Note: this can be a comma separated list of book names of which all will go through the screen capture step prior to the OCR step.  Capturing multiple books requires the user to wait for screen capture process (minutes) and then leave for the OCR step (which can take many hours).",
+    )
 
     # Optional argument for output directory
     parser.add_argument(
@@ -38,7 +38,11 @@ if __name__ == "__main__":
 
     # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
     parser.add_argument(
-        "-v", "--verbose", action="count", default=0, help="Verbosity (-v, -vv, etc)"
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Verbosity (-v, -vv, etc)",
     )
 
     # Specify output of "--version"
@@ -52,4 +56,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    solved = clone_ebook.clone_ebook_main(output_directory=args.output_dir,book_name=args.book_name, max_pages=args.max_pages)
+    book_names_no_spaces = args.book_name
+    book_name_lst = args.book_name.split(",")
+
+    solved = clone_ebook.clone_ebook_main(
+        output_directory=args.output_dir,
+        book_names_lst=args.book_name,
+        max_pages=args.max_pages,
+    )
